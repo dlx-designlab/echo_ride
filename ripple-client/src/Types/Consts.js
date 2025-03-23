5e9c66f3c4cd3f2d84cd1f5ccd24ef942ecdca34
yurikleb <yurikleb@gmail.com>
2025-03-23 13:28:19 +0000

    added github codespaces config

--- ripple-client/src/Types/Consts.js
+++ ripple-client/src/Types/Consts.js
@@ -2,7 +2,8 @@ import ip from '../ip';
 
 // export const ip = 'localhost';
 export const port = '3001';
-export const url = `http://${ip}:${port}/`;
+export const url = `https://${ip}/`; // <<< use this syntax when running on github codespaces
+// export const url = `https://${ip}:${port}/`; // <<< use this syntax when running locally
 
 export const busNumber = 5;
 
--- ripple-client/src/ip.js
+++ ripple-client/src/ip.js
@@ -1,1 +1,4 @@
-export default "192.168.0.108"; 
+// When running locally use local machine ip, 
+// When running on github codespaces use URL provided by Github Codespace server script with no "http" and "/" 
+// e.g.: ictional-meme-xxxxxxx-3001.app.github.dev
+export default "fictional-meme-69vgxrp79g6354w4-3001.app.github.dev";
